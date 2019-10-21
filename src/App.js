632fe4d15cae4ab65f03d17b3d47cf14e231aa19
Bibin1995 <56510124+Bibin1995@users.noreply.github.com>
2019-10-21 14:41:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import RegistrationForm from "./components/Home";
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<RegistrationForm />
+			</div>
+		);
+	}
+}
+
+export default App;;
